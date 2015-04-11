9bd621e183608bc3ce0918f85338f1861aa0734f
Javier Dorfsman <javitolin@gmail.com>
2015-04-11 18:36:24 +0300

    working??

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
  *      Author: jdorfsman
  */
 
+
+//////////////////// IMAGE SIZE: 253 * 62
 #include "highgui/highgui.hpp"
 #include "core/core.hpp"
 #include "imgproc/imgproc.hpp"
@@ -18,177 +20,188 @@
 using namespace cv;
 using namespace std;
 
-#define INTENSITY 255
+#define INTENSITY 256
+#define HISTMATCH_EPSILON 1.5
 Mat src, dst;
 int outputImageExists = 0;
 char* targetHistogramFile;
 char* targetImageFile;
 double receivedHistogram[256];
-float sk[256];
-double Gr[256];
-double Gb[256];
-double Gg[256];
+double sk[256];
+double Gz[256];
+double epsilon = 1.5;
 double histoMatchesR[256];
-double histoMatchesG[256];
-double histoMatchesB[256];
-double rHistogram[256];
-double bHistogram[256];
-double gHistogram[256];
-vector<Mat> bgr;
+double imageHistogram[256];
 
-void createDstImage(){
-	dst = Mat::zeros(src.rows,src.cols,CV_8UC3);
-	for(int i = 0; i < dst.rows; i++){
-		for(int j = 0; j < dst.cols; j++){
-			dst.at<Vec3b>(i,j)(0) = bgr[0].at<uchar>(i,j);
-			dst.at<Vec3b>(i,j)(2) = bgr[1].at<uchar>(i,j);
-			dst.at<Vec3b>(i,j)(1) = bgr[2].at<uchar>(i,j);
+void drawImages() {
+	dst = Mat::zeros(src.rows, src.cols, src.type());
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
+			dst.at < uchar > (i, j) = histoMatchesR[src.at < uchar > (i, j)];
 		}
 	}
 }
-void drawImages(Mat img, double toUse[]){
-	for(int i = 0; i < img.rows; i++){
-		for(int j = 0; j < img.cols; j++){
-			img.at<uchar>(i,j) = toUse[img.at<uchar>(i,j)];
-		}
-	}
-}
-void calculateValues(){
+void calculateValues() {
 	double sumForNowSK = 0;
-	double sumForNowGr = 0;
-	double sumForNowGb = 0;
-	double sumForNowGg = 0;
-	for(int i = 0; i < 256; i++){
-		sk[i] = (sumForNowSK + receivedHistogram[i])/(INTENSITY-1);
+	double sumForNowGz = 0;
+	for (int i = 0; i < INTENSITY; i++) {
+		sk[i] = (INTENSITY - 1) * imageHistogram[i] + sumForNowSK;
+		Gz[i] = (INTENSITY - 1) * receivedHistogram[i] + sumForNowGz;
 		sumForNowSK = sk[i];
-
-		Gr[i] = sumForNowGr + (INTENSITY-1) * rHistogram[i];
-		Gr[i] /= (src.rows*src.cols);
-		sumForNowGr = Gr[i];
-
-		Gg[i] = sumForNowGg + (INTENSITY-1) * gHistogram[i];
-		Gg[i] /= (src.rows*src.cols);
-		sumForNowGg = Gg[i];
-
-		Gb[i] = sumForNowGb + (INTENSITY-1) * bHistogram[i];
-		Gb[i] /= (src.rows*src.cols);
-		sumForNowGb = Gb[i];
+		sumForNowGz = Gz[i];
 	}
-	for(int i = 0; i < 256; i++){
-		sk[i] = sk[i] * INTENSITY;
-		sk[i] = round(sk[i]);
-		Gr[i] = round(Gr[i]);
-		Gg[i] = round(Gg[i]);
-		Gb[i] = round(Gb[i]);
+	for (int i = 0; i < INTENSITY; i++) {
+		sk[i] = sk[i] / (src.rows * src.cols);
+		Gz[i] = Gz[i] / (src.rows * src.cols);
+		sk[i] =
+				(round(sk[i]) < (INTENSITY - 1) ? round(sk[i]) : (INTENSITY - 1));
+		Gz[i] =
+				(round(Gz[i]) < (INTENSITY - 1) ? round(Gz[i]) : (INTENSITY - 1));
+		cout << "Gz: " << Gz[i] << " SK: " << sk[i] << endl;
 	}
+
 	int countMatches = 0;
-	for(int i = 0; i < 256; i++){
-		for(int j = 0; j < 256; j++){
-			if(sk[i] == Gr[j]){
-				histoMatchesR[j] += sk[i];
+	for (int i = 0; i < INTENSITY; i++) {
+		for (int j = 0; j < INTENSITY; j++) {
+			if (abs(sk[i] - Gz[j]) < epsilon) {
+				if (histoMatchesR[j] == 0) {
+					histoMatchesR[j] = sk[i];
+				}
 				countMatches++;
 			}
 		}
 	}
-	cout << "matches: " << countMatches << endl;
-	countMatches = 0;
-	for(int i = 0; i < 256; i++){
-		for(int j = 0; j < 256; j++){
-			if(sk[i] == Gg[j]){
-				histoMatchesG[j] += sk[i];
-				countMatches++;
-			}
-		}
-	}
-	cout << "matches: " << countMatches << endl;
-	countMatches = 0;
-	for(int i = 0; i < 256; i++){
-		for(int j = 0; j < 256; j++){
-			if(sk[i] == Gb[j]){
-				histoMatchesB[j] += sk[i];
-				countMatches++;
-			}
-		}
-	}
-	cout << "matches: " << countMatches << endl;
+	cout << "Matches: " << countMatches << endl;
 }
-void initializeHistograms(){
-	for(int i = 0; i < 256; i++){
-		rHistogram[i] = 0;
-		bHistogram[i] = 0;
-		gHistogram[i] = 0;
+/*
+ void calculateValues(){
+ double sumForNowSK = 0;
+ double sumForNowGr = 0;
+ for(int i = 0; i < 256; i++){
+ sk[i] = (sumForNowSK + receivedHistogram[i])/(INTENSITY-1);
+ Gz[i] = sumForNowGr + ((INTENSITY-1) * imageHistogram[i])/(src.rows*src.cols);
+ sumForNowSK = sk[i];
+ sumForNowGr = Gz[i];
+ }
+ for(int i = 0; i < 256; i++){
+ sk[i] = round(sk[i]);
+ Gz[i] = round(Gz[i]);
+ cout << "GR: " << Gz[i] << " SK: " << sk[i] << endl;
+ }
+ int countMatches = 0;
+ for(int i = 0; i < 256; i++){
+ for(int j = 0; j < 256; j++){
+ if(sk[i] == Gz[j]){
+ histoMatchesR[j] += sk[i];
+ countMatches++;
+ }
+ }
+ }
+ cout << "Matches: " << countMatches << endl;
+ }*/
+void initializeHistograms() {
+	for (int i = 0; i < 256; i++) {
+		imageHistogram[i] = 0;
 		histoMatchesR[i] = 0;
-		histoMatchesG[i] = 0;
-		histoMatchesB[i] = 0;
-		Gg[i] = 0;
-		Gr[i] = 0;
-		Gb[i] = 0;
+		Gz[i] = 0;
 		sk[i] = 0;
+		receivedHistogram[i] = 0;
 	}
 }
-void createHistogram(Mat img, double hist[]){
-	for(int i = 0; i < img.rows; i++){
-		for(int j = 0; j < img.cols; j++){
-			hist[img.at<uchar>(i, j)]++;
+void createHistogramFromImage() {
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
+			imageHistogram[src.at < uchar > (i, j)]++;
 		}
 	}
 }
 
-void readHistogram(){
+void readHistogramFromFile() {
 	ifstream myfile;
 	string line;
-	myfile.open (targetHistogramFile);
+	myfile.open(targetHistogramFile);
 	int lineNum = 0;
-	if (myfile.is_open())
-	  {
-	    while ( getline (myfile,line) )
-	    {
-	      receivedHistogram[lineNum++] = atoi(line.c_str());
-	    }
-	    myfile.close();
-	  }
+	if (myfile.is_open()) {
+		while (getline(myfile, line)) {
+			receivedHistogram[lineNum++] = atoi(line.c_str());
+		}
+		myfile.close();
+	}
 }
-void printHistogram(){
-	for(int i = 0; i < 256; i++){
+void printHistogram() {
+	for (int i = 0; i < 256; i++) {
 		cout << "Line: " << i << " Intensity: " << receivedHistogram[i] << endl;
 	}
 }
 
-int main(int argc, char * argv[]){
+int main(int argc, char * argv[]) {
 	char* inputImageFile;
-	if(argc < 3){
-		cout << "Usage is: MatchHistogram <input_image_file> <target_histogram> (output_image_file)?" << endl;
-	}
-	else if(argc == 3){
+	if (argc < 3) {
+		cout
+				<< "Usage is: MatchHistogram <input_image_file> <target_histogram> (output_image_file)?"
+				<< endl;
+	} else if (argc == 3) {
 		inputImageFile = argv[1];
 		targetHistogramFile = argv[2];
-	}
-	else if(argc == 4){
+	} else if (argc == 4) {
 		inputImageFile = argv[1];
 		targetHistogramFile = argv[2];
 		targetImageFile = argv[3];
 		outputImageExists = 1;
 	}
-	src = imread(inputImageFile,1);
-	readHistogram();
-	split(src, bgr);
+	src = imread(inputImageFile, 0);
 	initializeHistograms();
-	createHistogram(bgr[0],bHistogram);
-	createHistogram(bgr[1],gHistogram);
-	createHistogram(bgr[2],rHistogram);
-	calculateValues();
-	drawImages(bgr[0],histoMatchesB);
-	drawImages(bgr[1],histoMatchesG);
-	drawImages(bgr[2],histoMatchesR);
-	createDstImage();
-	namedWindow("Source Image", WINDOW_AUTOSIZE);  // Create a window for display.
+	createHistogramFromImage();
+	//Calculating cumulative histogram of src
+	double total = src.rows*src.cols;
+	double probSrc[255];
+	int newValuesSrc[255];
+	double cuml = 0;
+	for(int j = 0; j < 256; j++)
+	{
+		probSrc[j] = imageHistogram[j]/total; // Probability of each value in image
+	    cuml = cuml + probSrc[j]; // Cumulative probability of current and all previous values
+	    double cdfmax = cuml * 255;   // Cumulative probability * max value
+	    newValuesSrc[j] = (int) round(cdfmax);
+	    cout << imageHistogram[j] << " "<< probSrc[j] << " " << newValuesSrc[j] << endl;
+	}
+	readHistogramFromFile();
+	cout << "########################" << endl;
+	//Calculating cumulative histogram from file
+	double probDst[255];
+	int newValuesDst[255];
+	int maxNewValue = 0;
+	int sumOfHistogram = 0;
+	cuml = 0;
+	for(int j = 0; j < 256; j++)
+	{
+		probDst[j] = receivedHistogram[j]/total; // Probability of each value in image
+	    cuml = cuml + probDst[j]; // Cumulative probability of current and all previous values
+	    double cdfmax = cuml * 255;   // Cumulative probability * max value
+	    newValuesDst[j] = (int) round(cdfmax);
+	    sumOfHistogram+=receivedHistogram[j];
+	    cout << receivedHistogram[j] << " "<< probDst[j] << " " << newValuesDst[j] << endl;
+	}
+	cout << sumOfHistogram << endl;
+	for (int i = 0; i < 256; i++) {
+		for (int j = 0; j < 256; j++) {
+			if (newValuesDst[i] == newValuesSrc[j]) {
+				histoMatchesR[j] += newValuesSrc[i];
+			}
+		}
+	}
+	drawImages();
+
+
+	//calculateValues();
+
+	namedWindow("Source Image", WINDOW_AUTOSIZE); // Create a window for display.
 	imshow("Source Image", src);  // Show Before Image
 	namedWindow("Dst Image", WINDOW_AUTOSIZE);  // Create a window for display.
 	imshow("Dst Image", dst);  // Show Before Image
-	if(outputImageExists == 1){
-		imwrite(targetImageFile,dst);
+	if (outputImageExists == 1) {
+		imwrite(targetImageFile, dst);
 	}
 	waitKey();
 	return 0;
 }
-
